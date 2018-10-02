0e725b4560721b942f5a625d90a723bb0f14d0be
marti <marti@DESKTOP-K6KDR87>
2018-10-03 01:55:30 +0300

    version 1.0

--- factorial_draw/Form1.Designer.cs
+++ factorial_draw/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.drawp = new System.Windows.Forms.Panel();
+            this.drawpic = new System.Windows.Forms.PictureBox();
+            this.num = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.drawp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.drawpic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "erase";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // drawp
+            // 
+            this.drawp.AutoScroll = true;
+            this.drawp.Controls.Add(this.drawpic);
+            this.drawp.Location = new System.Drawing.Point(37, 55);
+            this.drawp.Name = "drawp";
+            this.drawp.Size = new System.Drawing.Size(1235, 614);
+            this.drawp.TabIndex = 2;
+            // 
+            // drawpic
+            // 
+            this.drawpic.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.drawpic.Location = new System.Drawing.Point(15, 13);
+            this.drawpic.Name = "drawpic";
+            this.drawpic.Size = new System.Drawing.Size(1217, 598);
+            this.drawpic.TabIndex = 0;
+            this.drawpic.TabStop = false;
+            this.drawpic.Click += new System.EventHandler(this.drawpic_Click);
+            // 
+            // num
+            // 
+            this.num.AllowDrop = true;
+            this.num.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.num.Location = new System.Drawing.Point(209, 25);
+            this.num.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.num.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(60, 20);
+            this.num.TabIndex = 4;
+            this.num.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(275, 17);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(168, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(474, 22);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(233, 23);
+            this.progressBar1.Step = 50;
+            this.progressBar1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(745, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "AntiAlias";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1284, 681);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.num);
+            this.Controls.Add(this.drawp);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.drawp.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.drawpic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel drawp;
+        private System.Windows.Forms.PictureBox drawpic;
+        private System.Windows.Forms.NumericUpDown num;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- factorial_draw/Form1.cs
+++ factorial_draw/Form1.cs
@@ -15,6 +15,133 @@ namespace factorial_draw
         public Form1()
         {
             InitializeComponent();
+            drawpic.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.trackBar1.MouseCaptureChanged += new System.EventHandler(this.dr);
+        }
+        List<Point> points = new List<Point>();
+        private void drawpic_Click(object sender, EventArgs e)
+        {
+            int x0 = drawpic.Right / 2, y0 = drawpic.Bottom / 2;
+            int xmax = drawpic.Right, ymax = drawpic.Bottom;
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coord = me.Location;
+            Brush aBrush = (Brush)Brushes.Black;
+            points.Add(coord);
+        //   g.FillRectangle(aBrush, (float)coord.X+x0, (-(float)coord.Y) +y0, 1, 1);
+            drawpic.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(points.Count>1)
+            drawing = true;
+            drawpic.Invalidate();
+       //  drawpic.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+        }
+
+        bool drawing = false;
+        private Font fnt = new Font("Arial", 10);
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Random rand = new Random();
+            Pen pen = new Pen(Color.FromArgb(200, 0, 0, 0));
+            Brush aBrush = (Brush)Brushes.Black;   
+            Graphics g = e.Graphics;
+            if (checkBox1.Checked)
+            {
+           g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            }
+            int x0 = drawpic.Right / 2, y0 = drawpic.Bottom / 2;
+            int xmax = drawpic.Right, ymax = drawpic.Bottom;
+
+            for (int i = 1; i < ymax / 2; i++)
+            {
+                for (int x = 0; x <= xmax / 2; x++)
+                { 
+                      //  g.FillRectangle(aBrush, (float)x,  i, 1, 1);
+                }
+
+            }
+
+            for (int t = 0; t < points.Count; t++)
+            {
+            //    e.Graphics.DrawLine(pen, (float)points[t].X + x0, (-(float)points[t].Y) + y0, (float)points[t].X + x0+10, (-(float)points[t].Y) + y0+10);
+                g.FillRectangle(aBrush, points[t].X, (points[t].Y) , 2, 2);
+            }
+            int k = count;
+            if(drawing)
+            {
+              
+                int pos2 = rand.Next(0, points.Count);
+                Point midl = new Point();
+                Point last = new Point();
+                while (0==pos2)
+                        pos2 = rand.Next(0, points.Count);
+                midl = getmiddle(points[0], points[pos2]);
+                while (k!=0)
+                {
+                    if ((k % 10) == 0)
+                    {
+                        int p = ( k*100) /count;
+                     progressBar1.Value = p;
+               
+                    }
+               
+                 
+                    g.FillRectangle(aBrush, midl.X, (midl.Y), 1, 1);
+                    last = midl;
+                    pos2 = rand.Next(0, points.Count);
+                    midl = getmiddle(last, points[pos2]);
+                    k--;
+                }
+            }
+            drawing = false;
+            //   g.FillRectangle(aBrush, (float)buf.x1+x0, (-(float)buf.x2)+y0, 1, 1);
+
+        }
+        Point getmiddle (Point a, Point b)
+        {
+            Point midl = new Point();
+            midl.X = (a.X + b.X) / 2;
+            midl.Y = (a.Y + b.Y) / 2;
+            return midl;
+        }
+        private void drawp_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            drawpic.Invalidate();
+        }
+        int count = 1000; int kof = 2000;
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            count = kof * (int)num.Value; 
+        }
+     
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            count = kof * trackBar1.Value;
+            num.Value = trackBar1.Value * 10;
+        }
+        private void dr(object sender, EventArgs e)
+        {
+drawin();
+        }
+        void drawin()
+        {
+            if (points.Count > 1)
+                drawing = true;
+            drawpic.Invalidate();
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            drawin();
         }
     }
 }
